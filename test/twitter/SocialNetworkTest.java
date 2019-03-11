07224fd4ca77342ff577b4daa7b5bc2d86084c0e
yusrants <47317172+yusrants@users.noreply.github.com>
2019-03-11 23:08:31 +0500

    Update SocialNetworkTest.java

--- test/twitter/SocialNetworkTest.java
+++ test/twitter/SocialNetworkTest.java
@@ -49,21 +49,21 @@ public class SocialNetworkTest {
 
     }
     @Test
-    public void testGuessFollowsGraphEmpty() {
+    public void test1GuessFollowsGraph() {
         Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1,tweet2,tweet3,tweet4));
         
         assertTrue("expected followers list", followsGraph.get("Ernie").contains("Bert"));
 
     }
     @Test
-    public void testGuessFollowsGraphEmpty() {
+    public void test2GuessFollowsGraph() {
         Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1,tweet2,tweet3,tweet4));
 
 
         assertTrue("expected empty graph", followsGraph.get("Ernie").contains("Zaeem"));
     }
     @Test
-    public void testGuessFollowsGraphEmpty() {
+    public void test3GuessFollowsGraph() {
         Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1,tweet2,tweet3,tweet4));
 
 
